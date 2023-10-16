995eb54ac38ebb71e9c2f7b701e95fd9cc47f8c1
earonesty <erik@q32.com>
2023-10-16 16:30:12 -0400

    ps (#8)

--- ai_spider/stats.py
+++ ai_spider/stats.py
@@ -1,7 +1,13 @@
+import contextlib
+import logging
 import math
 import random
 import time
 from collections import defaultdict
+from queue import Empty
+from threading import Thread
+
+from ai_spider.unique_queue import UniqueQueue
 
 STATS_EMA_ALPHA = 0.9
 PUNISH_SECS = 60 * 15
@@ -9,8 +15,8 @@ PUNISH_BAD_PERF = 9999
 
 
 class StatsBin:
-    def __init__(self, alpha):
-        self.val = None
+    def __init__(self, alpha, val=None):
+        self.val = val
         self.alpha = alpha
 
     def bump(self, secs):
@@ -22,31 +28,32 @@ class StatsBin:
 
 class StatsWorker:
     def __init__(self, alpha):
-        self.stats: dict[int, StatsBin] = defaultdict(lambda: StatsBin(alpha))
+        self.alpha = alpha
+        self.msize_stats: dict[int, StatsBin] = defaultdict(lambda: StatsBin(alpha))
         self.bad = None
         self.cnt = 0
 
     def bump(self, msize, usage, secs):
         self.cnt += 1
-        toks = usage.get("total_tokens")
+        toks = usage["total_tokens"]
         # similar-sized models are lumped together
         msize_bin = round(math.sqrt(msize))
         # similar-sized token counts are lumped together too
-        self.stats[msize_bin].bump(secs / toks)
+        self.msize_stats[msize_bin].bump(secs / toks)
         # all is forgiven
         self.bad = None
 
     def perf(self, msize):
         if self.bad and self.bad > time.monotonic():
             return PUNISH_BAD_PERF
-        if not self.stats:
+        if not self.msize_stats:
             return None
         msize_bin = round(math.sqrt(msize))
-        exact = self.stats.get(msize_bin)
+        exact = self.msize_stats.get(msize_bin)
         if exact:
             return exact.val
-        close_bin = sorted(self.stats.keys())[-1]
-        close = self.stats.get(close_bin)
+        close_bin = sorted(self.msize_stats.keys())[-1]
+        close = self.msize_stats.get(close_bin)
         # adjustment for model size is (msize/closest_size) ** 1.5
         if msize_bin < close_bin:
             approx = close.val * (msize / (close_bin ** 2.0)) ** 1.5
@@ -58,28 +65,114 @@ class StatsWorker:
     def punish(self, secs):
         self.bad = time.monotonic() + secs
 
+    def dump(self):
+        return dict(
+            dat={k: v.val for k, v in self.msize_stats.items()},
+            cnt=self.cnt
+        )
+
+    def load(self, dct):
+        self.cnt = dct["cnt"]
+        self.msize_stats = {k: StatsBin(self.alpha, v) for k, v in dct["dat"].items()}
+
 
 # 2 == very skewed (prefer first, but the rest are skewed to the front)
 # 1 == not skewed (prefer first, but the rest are even)
 
 POWER = 2
 
+DEFAULT_BATCH_COUNT = 100
+
+
+class StatsStore:
+    def __init__(self):
+        self.done = False
+        self.batch_count = DEFAULT_BATCH_COUNT
+        self.update_queue = UniqueQueue(key=lambda el: el[0])
+        self.update_thread = Thread(daemon=True, target=self.update_loop)
+        self.update_thread.start()
+
+    def stop(self, join=True):
+        self.done = True
+        self.update_queue.put([None, None])
+        if join:
+            self.update_thread.join()
+
+    def wait(self):
+        while not self.update_queue.empty():
+            time.sleep(0.1)
+
+    def update_loop(self):
+        while not self.done:
+            try:
+                with self._transaction():
+                    for _ in range(self.batch_count):
+                        key, val = self.update_queue.get(timeout=4)
+                        if key is None:
+                            self.done = True
+                            break
+                        try:
+                            self._update(key, val)
+                        except Exception as ex:
+                            logging.error("error updating db: %s", repr(ex))
+                            self.update_queue.put((key, val))
+                            time.sleep(1)
+            except Empty:
+                pass
+
+    def update(self, key: str, val: dict):
+        self.update_queue.put([key, val])
+
+    def _update(self, key, val):
+        raise NotImplementedError
+
+    def get(self, key: str) -> dict:
+        raise NotImplementedError
+
+    @contextlib.contextmanager
+    def _transaction(self):
+        yield
+
 
 class StatsContainer:
-    def __init__(self, alpha=STATS_EMA_ALPHA, key=None):
-        self.stats: dict[str, StatsWorker] = defaultdict(lambda: StatsWorker(alpha))
+    ALL_KEY = "<all>"
+
+    def __init__(self, alpha=STATS_EMA_ALPHA, key=None, store: StatsStore = None):
+        self.worker_stats: dict[str, StatsWorker] = defaultdict(lambda: StatsWorker(alpha))
         self.all = StatsWorker(alpha)
         ident = lambda k: k
         self.key_func = key or ident
+        self.store = store
+        self.load(self.ALL_KEY)
+
+    def load(self, key):
+        if self.store and isinstance(key, str):
+            dat = self.store.get(key)
+            if dat:
+                self.worker_stats[key].load(dat)
+                return self.worker_stats.get(key)
 
     def bump(self, key, msize, usage, secs):
         key = self.key_func(key)
-        self.stats[key].bump(msize, usage, secs)
+        self.worker_stats[key].bump(msize, usage, secs)
+
+        if self.store and isinstance(key, str):
+            self.store.update(key, self.worker_stats[key].dump())
+
         self.all.bump(msize, usage, secs)
 
-    def perf(self, key, msize):
+        if self.store:
+            self.store.update(self.ALL_KEY, self.all.dump())
+
+    def get(self, key):
         key = self.key_func(key)
-        wrk = self.stats.get(key)
+        wrk = self.worker_stats.get(key)
+        if not wrk:
+            wrk = self.load(key)
+        return wrk
+
+    def perf(self, key, msize):
+        wrk = self.get(key)
         if wrk:
             ret = wrk.perf(msize)
             if ret:
@@ -94,15 +187,15 @@ class StatsContainer:
         return ordered[pick][0]
 
     def punish(self, key, secs=PUNISH_SECS):
-        key = self.key_func(key)
-        wrk = self.stats.get(key)
+        wrk = self.get(key)
         if wrk:
             wrk.punish(secs)
 
     def cnt(self, key):
-        key = self.key_func(key)
-        wrk = self.stats.get(key)
+        wrk = self.get(key)
         if wrk:
             return wrk.cnt
         return 0
 
+    def drop(self, key):
+        self.worker_stats.pop(key, None)
