cff73e7f3fde3a6f2b4f3a164c4e764d38654570
erik aronesty <erik@q32.com>
2023-10-20 16:47:51 -0400

    runt

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python -m coverage run --source ai_spider -m pytest tests/
+poetry run python -mcoverage run --source ai_spider -m pytest tests/
 coverage html
 coverage report --fail-under 77
