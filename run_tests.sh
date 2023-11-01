a9bf164c3fce1d75f94bcfe6f0936fbc08658ed5
erik aronesty <erik@q32.com>
2023-11-01 13:12:50 -0400

    .

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
 poetry run python -u -mcoverage run --source ai_spider -m pytest -v tests/
+
+# these tests run ok manually
 poetry run python -u -mcoverage run -a --source ai_spider -m pytest -v tests/ -k test_websocket_xx --manual 
+
+# embedding live tests and other tests require cuda 
+
 coverage html
 coverage report --fail-under 77
